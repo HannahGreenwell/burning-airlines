c6ebd642a0b0b4abfee3d512237ef0068d4a58c2
Hannah <hannah.greenwell@gmail.com>
2018-10-31 09:20:32 +1100

    Fixing migration merge conflict

--- burning-airlines-server/db/migrate/20181030094516_change_column_name.rb
+++
@@ -1,5 +0,0 @@
-class ChangeColumnName < ActiveRecord::Migration[5.2]
-  def change
-    rename_column :flights, :plane_id, :airplane_id
-  end
-end
--- burning-airlines-server/db/schema.rb
+++ burning-airlines-server/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_30_094516) do
+ActiveRecord::Schema.define(version: 2018_10_30_054522) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,7 +28,7 @@ ActiveRecord::Schema.define(version: 2018_10_30_094516) do
     t.text "date"
     t.text "destination"
     t.text "origin"
-    t.integer "airplane_id"
+    t.integer "plane_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
