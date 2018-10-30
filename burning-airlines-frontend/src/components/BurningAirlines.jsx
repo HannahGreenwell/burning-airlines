679061fdbd5013534b90a02ff4d2cf7e2f586524
Hannah <hannah.greenwell@gmail.com>
2018-10-31 10:39:31 +1100

    React set-up done and added BurningAirlines component

---
+++ burning-airlines-frontend/src/components/BurningAirlines.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class BurningAirlines extends Component {
+  render() {
+    return (
+      <h1>BurningAirlines Component</h1>
+    );
+  }
+}
+
+export default BurningAirlines;
