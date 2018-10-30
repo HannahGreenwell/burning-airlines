4ad1f69b6f451969efb9e646fd5476a2ab3add1b
Brad Dong <braddong@hotmail.com>
2018-10-30 21:23:21 +1100

    airplane new show done

---
+++ burning-airlines-server/test/controllers/airplanes_controller_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class AirplanesControllerTest < ActionDispatch::IntegrationTest
+  test "should get new" do
+    get airplanes_new_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get airplanes_create_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get airplanes_show_url
+    assert_response :success
+  end
+
+  test "should get index" do
+    get airplanes_index_url
+    assert_response :success
+  end
+
+end
