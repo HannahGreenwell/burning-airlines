262c6727acf27966dde2d9b42c23893cae482e02
Hannah <hannah.greenwell@gmail.com>
2018-10-31 09:27:15 +1100

    Put flight routes back in

--- burning-airlines-server/config/routes.rb
+++ burning-airlines-server/config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
 
   resources :airplanes
 
+  get '/flights' => 'flights#new'
+  post '/flights' => 'flights#create'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
