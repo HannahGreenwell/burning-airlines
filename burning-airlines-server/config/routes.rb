f7dabc6a0ae20879e869ef339e5f4b6b6dd87ce7
Hannah <hannah.greenwell@gmail.com>
2018-10-31 09:58:58 +1100

    Updated routes.rb

--- burning-airlines-server/config/routes.rb
+++ burning-airlines-server/config/routes.rb
@@ -4,6 +4,12 @@ Rails.application.routes.draw do
 
   get '/flights' => 'flights#new'
   post '/flights' => 'flights#create'
+  get '/flight/:id' => 'flight#show'
+
+  get '/search' => 'flights#search'
+
+  get '/reservations' => 'reservations#new'
+  get '/reservations' => 'reservations#show'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
