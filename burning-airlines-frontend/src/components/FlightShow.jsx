ac14e070904a99c93d7a21a767e9df29a6a63e0b
Hannah <hannah.greenwell@gmail.com>
2018-10-31 21:02:47 +1100

    Added functionality to show whether a seat is reserved or available

--- burning-airlines-frontend/src/components/FlightShow.jsx
+++ burning-airlines-frontend/src/components/FlightShow.jsx
@@ -2,7 +2,8 @@ import React, {Component} from 'react';
 
 import axios from 'axios';
 
-// const BA_URL = 'http://localhost:3000/flight/';
+import SeatMap from './SeatMap';
+
 
 class FlightShow extends Component {
 
@@ -51,54 +52,17 @@ class FlightShow extends Component {
     .catch(console.warn)
   }
 
-  renderSeat(row, column) {
-    const seatWidthPercentage = `${(100 / this.state.columns) - 1}%`;
-    let text = 'avail';
-    let className = 'seat avail';
-
-    if (this.state.reservedSeats.includes([parseInt(row), parseInt(column)])) {
-      text = '';
-      className = 'seat reserved';
-    }
-
-    return (
-      <div
-        className={className}
-        row={row}
-        column={column}
-        style={{width: seatWidthPercentage}}
-      >
-      {text}
-      </div>
-    );
-  }
-
-  renderSeatMap() {
-    let seatMap = [];
-
-    for (let i = 1; i <= this.state.rows; i++) {
-      let seats = [];
-
-      for (let j = 1; j <= this.state.columns; j++) {
-        seats.push(
-          this.renderSeat(i, j)
-        );
-      }
-      seatMap.push(<div className="row">{seats}</div>);
-    }
-
-    return seatMap;
-  }
-
   render() {
     return (
       <div className="flight_show">
         <p>{this.state.date} Flight {this.state.flightNum} {this.state.origin} - {this.state.destination}</p>
 
         <div className="seatmap-container">
-          <div className="seatmap">
-            {this.renderSeatMap()}
-          </div>
+          <SeatMap
+            numOfRows={this.state.rows}
+            numOfColumns={this.state.columns}
+            reservedSeats={this.state.reservedSeats}
+          />
         </div>
       </div>
     );
