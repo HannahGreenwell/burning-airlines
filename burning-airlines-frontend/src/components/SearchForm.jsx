bd798667b00329accb50c2953357b5ea8b1b6e29
Brad Dong <braddong@hotmail.com>
2018-10-31 15:58:30 +1100

    search page in progress

--- burning-airlines-frontend/src/components/SearchForm.jsx
+++ burning-airlines-frontend/src/components/SearchForm.jsx
@@ -1,9 +1,40 @@
 import React, {Component} from 'react';
+import axios from 'axios'
+
+const URL = 'http://localhost:3000/flights';
 
 class SearchForm extends Component {
+
+  constructor(props){
+    super(props)
+    this.state={
+    flights: []
+    }
+  }
+
+  componentDidMount(){
+    this.fetchFlights();
+    setInterval(()=>this.fetchFlights(),2000);
+  }
+
+  fetchFlights(){
+    axios.get(URL)
+    .then(response=>{
+      console.log(response);
+
+    })
+  }
+
+
+
   render() {
     return (
-      <h2>SearchForm Component</h2>
+      <div>
+        <ul>
+          { this.state.flights.map( s => <li key={s.id}>{ s.flight_id }</li> ) }
+        </ul>
+      </div>
+
     );
   }
 }
