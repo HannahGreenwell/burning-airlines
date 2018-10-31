f7a0da53f7602df14fa48ea668952f787ff142cf
Hannah <hannah.greenwell@gmail.com>
2018-10-31 12:11:31 +1100

    Halfway through seatmap

--- burning-airlines-frontend/src/components/FlightShow.jsx
+++ burning-airlines-frontend/src/components/FlightShow.jsx
@@ -1,9 +1,23 @@
 import React, {Component} from 'react';
 
 class FlightShow extends Component {
+  renderSeatMap(row, column) {
+
+  }
+
   render() {
     return (
-      <h2>FlightShow Component</h2>
+      <div class="flight_show">
+        <h2>FlightShow Component</h2>
+
+        <p>3/18/13 Flight 09 JFK > SFO</p>
+
+        <div className="seatmap-container">
+          <div className="seatmap">
+
+          </div>
+        </div>
+      </div>
     );
   }
 }
