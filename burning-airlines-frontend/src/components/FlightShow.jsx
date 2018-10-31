4b068b0aa9a98f37a0be61ed2578c76aa6ec5ab4
Hannah <hannah.greenwell@gmail.com>
2018-10-31 14:55:49 +1100

    Added renderSeatMap method to FlightShow component and updated index.css

--- burning-airlines-frontend/src/components/FlightShow.jsx
+++ burning-airlines-frontend/src/components/FlightShow.jsx
@@ -1,20 +1,43 @@
 import React, {Component} from 'react';
 
 class FlightShow extends Component {
-  renderSeatMap(row, column) {
 
+  constructor() {
+    super();
+
+    this.state = {
+      rows: 4,
+      columns: 2,
+    };
+  }
+
+  renderSeatMap() {
+    // console.log('Rows: ', this.state.rows);
+    // console.log('Columns: ', this.state.columns);
+
+    let seatMap = [];
+
+    for (let i = 0; i < this.state.rows; i++) {
+      let seats = [];
+      for (let j = 0; j < this.state.columns; j++) {
+        seats.push(<div className="seat" row={i} column={j}>avail</div>);
+      }
+      seatMap.push(<div className="row">{seats}</div>);
+    }
+
+    return seatMap;
   }
 
   render() {
     return (
-      <div class="flight_show">
+      <div className="flight_show">
         <h2>FlightShow Component</h2>
 
         <p>3/18/13 Flight 09 JFK > SFO</p>
 
         <div className="seatmap-container">
           <div className="seatmap">
-
+            {this.renderSeatMap()}
           </div>
         </div>
       </div>
